8736baf1338538cf4c6658fed85126c504bb8e67
tbranquinho18 <tiagodsbranquinho@tecnico.ulisboa.pt>
2023-12-23 15:31:57 +0000

    --almost p3

--- projeto2/p2.cpp
+++ projeto2/p2.cpp
@@ -7,57 +7,45 @@ using namespace std;
 
 const int UNVISITED = -1;
 
+void dfs(int start, const vector<vector<int>>& graph, vector<int>& ids, vector<int>& low, vector<int>& sccs, vector<bool>& onStack, stack<int>& stackOriginal, int& id, int& sccCount) {
+    stack<int> dfsStack;
+    dfsStack.push(start);
 
-int dfs(const vector<vector<int>>& graph, vector<int>& ids, vector<int>& low, vector<int>& sccs, vector<bool>& visited, stack<int>& nodeStack, int& id) {
-  int n = graph.size();
-  int sccCount = 0;
-
-  for (int start = 0; start < n; ++start) {
-    if (ids[start] == UNVISITED) {
-      nodeStack.push(start);
-
-      while (!nodeStack.empty()) {
-        int at = nodeStack.top();
-
-        if (!visited[at]) {
-          // White node
-          ids[at] = low[at] = id++;
-          visited[at] = true;
-        } else {
-          // Gray node
-          for (int i = 0; i < (int) graph[at].size(); ++i) {
-            int to = graph[at][i];
-            if (to >= n) {
-              // Ignore nodes with indices greater than or equal to n
-              continue;
-            }
-            if (ids[to] == UNVISITED) {
-              nodeStack.push(to);
-              break;
-            }
-            if (visited[to]) {
-              low[at] = min(low[at], low[to]);
-            }
-          }
-
-          if (ids[at] == low[at]) {
-            // Black node
-            while (!nodeStack.empty()) {
-              int node = nodeStack.top();
-              nodeStack.pop();
-              visited[node] = false;
-              sccs[node] = sccCount;
-              if (node == at) {
-                break;
-              }
-            }
-            sccCount++;
-          }
+    while (!dfsStack.empty()) {
+        int at = dfsStack.top();
+        if (ids[at] == UNVISITED) {
+            ids[at] = low[at] = id++;
+            stackOriginal.push(at);
+            onStack[at] = true;
         }
-      }
+
+        bool pushed = false;
+        for (int to : graph[at]) {
+            if (ids[to] == UNVISITED) {
+                dfsStack.push(to);
+                pushed = true;
+                break;
+            } else if (onStack[to]) {
+                low[at] = min(low[at], low[to]);
+            }
+        }
+
+        if (!pushed) {
+            if (ids[at] == low[at]) {
+                while (!stackOriginal.empty()) {
+                    int node = stackOriginal.top();
+                    stackOriginal.pop();
+                    onStack[node] = false;
+                    sccs[node] = sccCount;
+                    if (node == at) {
+                        break;
+                    }
+                }
+                ++sccCount;
+            }
+            dfsStack.pop();
+        }
     }
-  }
-  return sccCount;
 }
 
 int tarjanSccSolver(const vector<vector<int>>& graph, vector<int>& sccs) {
@@ -65,26 +53,26 @@ int tarjanSccSolver(const vector<vector<int>>& graph, vector<int>& sccs) {
   vector<int> ids(n, UNVISITED);
   vector<int> low(n, 0);
   vector<bool> visited(n, false);
-  stack<int> stack;
+  stack<int> stackOriginal;
   int id = 0;
   int sccCount = 0;
 
-  sccCount = dfs(graph, ids, low, sccs, visited, stack, id);
+  for (int i = 0; i < n; i++) {
+    if (ids[i] == UNVISITED) {
+      dfs(i, graph, ids, low, sccs, visited, stackOriginal, id, sccCount);
+    }
+  }
 
   return sccCount;
 }
 
-
 vector<vector<int>> buildSCCGraph(const vector<vector<int>>& graph, const vector<int>& sccs, int sccCount) {
   vector<vector<int>> sccGraph(sccCount);
 
-  for (size_t u = 0; u < graph.size(); ++u) {
-    for (int v : graph[u]) {
-      if (v >= (int)graph.size() || u >= sccs.size() || v >= (int)sccs.size()) {
-        continue;
-      }
+  for (std::vector<int>::size_type u = 0; u < graph.size(); ++u) {
+    for (std::vector<int>::size_type v : graph[u]) {
       if (sccs[u] != sccs[v]) {
-        sccGraph[sccs[u]].push_back(sccs[v]);
+          sccGraph[sccs[u]].push_back(sccs[v]);
       }
     }
   }
@@ -92,49 +80,44 @@ vector<vector<int>> buildSCCGraph(const vector<vector<int>>& graph, const vector
 }
 
 int findLongestRoute(const vector<vector<int>>& sccGraph, int source, vector<int>& dp) {
-  if (source >= (int)sccGraph.size() || dp[source] != -1) {
-    return dp[source];
-  }
+    if (dp[source] != -1) {
+        return dp[source];
+    }
 
-  int longestRoute = 0;
+    int longestRoute = 0;
 
-  for (int neighbor : sccGraph[source]) {
-    longestRoute = max(longestRoute, 1 + findLongestRoute(sccGraph, neighbor, dp));
-  }
+    for (int neighbor : sccGraph[source]) {
+        longestRoute = max(longestRoute, 1 + findLongestRoute(sccGraph, neighbor, dp));
+    }
 
-  dp[source] = longestRoute;
-  return longestRoute;
+    dp[source] = longestRoute;
+    return longestRoute;
 }
 
 int main() {
   int vertices, edges;
-  if (scanf("%d %d", &vertices, &edges) == EOF || vertices <= 0) {
-    printf("Invalid input.\n");
-    return 1;
-  }
+  if (scanf("%d %d", &vertices, &edges) == EOF) 
+    return 0;
 
   vector<vector<int>> graph(vertices);
 
   for (int i = 0; i < edges; ++i) {
     int u, v;
-    if (scanf("%d %d", &u, &v) == EOF || u <= 0 || v <= 0 || u > vertices || v > vertices) {
-      printf("Invalid input.\n");
-      return 1;
-    }
+    if (scanf("%d %d", &u, &v) == EOF) 
+      return 0;
     graph[u - 1].push_back(v - 1); // Adjust to 0-based indexing
   }
 
   vector<int> sccs(vertices, 0);
   int sccCount = tarjanSccSolver(graph, sccs);
 
-  // Build the new graph of SCCs
   vector<vector<int>> sccGraph = buildSCCGraph(graph, sccs, sccCount);
 
   vector<int> dp(sccCount, -1);
   int longestRoute = 0;
 
   for (int i = 0; i < sccCount; ++i) {
-    longestRoute = max(longestRoute, findLongestRoute(sccGraph, i, dp));
+      longestRoute = max(longestRoute, findLongestRoute(sccGraph, i, dp));
   }
 
   printf("%d\n", longestRoute);
--- projeto3/p3.py
+++ projeto3/p3.py
@@ -1,20 +1,53 @@
+from pulp import LpProblem, LpVariable, lpSum, value, LpMaximize
+
 def get_inputs():
     base = list(map(int, input().split()))
-    n, p, max = base[0], base[1], base[2]
+    n, p, max_toys = base[0], base[1], base[2]
     products = []
     specials = []
     for _ in range(n):
         products.append(list(map(int, input().split())))
     for _ in range(p):
         specials.append(list(map(int, input().split())))
-    return n, p, max, products, specials
+    return n, p, max_toys, products, specials
 
-def calculate():
-    pass
+def calculate(n, p, max_toys, products, specials):
+    # Create a LP problem
+    prob = LpProblem("MaximizeProfit", LpMaximize)
+
+    # Define variables
+    toy_vars = [LpVariable(f"x{i}", lowBound=0, cat="Integer") for i in range(n)]
+    special_vars = [LpVariable(f"sp{i}", lowBound = 0, cat="Integer") for i in range(p)]
+
+    # maximize total profit
+    prob += lpSum(products[i][0] * toy_vars[i] for i in range(n)) + \
+            lpSum(specials[i][3] * special_vars[i] for i in range(p)), "Total_Profit"
+    
+    prob += lpSum(toy_vars) + lpSum(special_vars)*3 <= max_toys
+
+    # production limits for individual toys
+    for i in range(n):
+        prob += toy_vars[i] >= 0
+        prob += toy_vars[i] <= products[i][1]
+
+    # special packages must contain the required individual toys
+    for i in range(p):
+        for j in specials[i][:3]:
+            prob += special_vars[i] <= toy_vars[j-1]
+
+    # Solve the problem
+    prob.solve()
+
+    # Print the optimal values
+    for v in prob.variables():
+        print(v.name, "=", v.varValue)
+
+    # Return the result
+    return value(prob.objective)
 
 def main():
-    n, p, max, products, specials = get_inputs()
-    result = calculate()
+    n, p, max_toys, products, specials = get_inputs()
+    result = calculate(n, p, max_toys, products, specials)
     print(result)
 
 main()
